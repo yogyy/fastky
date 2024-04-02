d4a9d438e4c496e78d41bba851b5ec54ad3a3160
yogyy <yogimr261@gmail.com>
2024-04-02 22:05:32 +0700

    feat: add auto load

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,10 +1,22 @@
 import fastify from "fastify";
 import { logger } from "./logger";
+import fastifyAutoload from "@fastify/autoload";
+import { join } from "path";
 
 export async function buildServer() {
   const app = fastify({
     logger,
   });
+
+  app
+    .register(fastifyAutoload, {
+      dir: join(process.cwd(), "src/plugins"),
+      options: Object.assign({}),
+    })
+    .register(fastifyAutoload, {
+      dir: join(process.cwd(), "src/routes"),
+      options: { prefix: "/api" },
+    });
 
   return app;
 }
