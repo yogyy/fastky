f3951cf4344a7b0d542b25e78ad19fb6db964cd7
yogyy <yogimr261@gmail.com>
2024-04-05 01:27:53 +0700

    feat: add schema for delete user

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -4,4 +4,9 @@ export const getUser = Type.Object({
   username: Type.String({ title: "username" }),
 });
 
+export const deleteUser = Type.Object({
+  email: Type.String({ format: "email" }),
+});
+
+export type deleteUserType = Static<typeof deleteUser>;
 export type getUserType = Static<typeof getUser>;
